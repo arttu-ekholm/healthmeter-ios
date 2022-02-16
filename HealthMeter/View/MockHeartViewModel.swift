da513860426ac04c8b6d40e0679e01f536311565
Arttu Ekholm <arttu.ekholm@bitodi.fi>
2022-02-16 15:23:04 +0200

    Implementation for mock view model

--- HealthMeter/View/MockHeartViewModel.swift
+++ HealthMeter/View/MockHeartViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class MockHeartRateService: RestingHeartRateService {
+    var average: Double?
+    override var averageHeartRate: Double? {
+        get { return average }
+        set { average = newValue }
+    }
+
+    override func queryAverageRestingHeartRate(averageRHRCallback: @escaping (Result<Double, Error>) -> Void) {
+        averageRHRCallback(.success(56.0))
+    }
+
+    override func queryLatestRestingHeartRate(completionHandler: @escaping (Result<RestingHeartRateUpdate, Error>) -> Void) {
+        completionHandler(.success(RestingHeartRateUpdate(date: .now, value: 61.0)))
+    }
+}
