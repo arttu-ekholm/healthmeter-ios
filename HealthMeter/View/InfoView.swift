0727858c6021e3f01a950d865b0902604784e135
Arttu Ekholm <arttu.ekholm@bitodi.fi>
2022-02-22 10:13:36 +0200

    Move view models to their own classes

--- HealthMeter/View/InfoView.swift
+++ HealthMeter/View/InfoView.swift
@@ -11,99 +11,6 @@ struct InfoView: View {
     @Environment(\.dismiss) var dismiss
     @StateObject var viewModel: ViewModel
 
-    class ViewModel: ObservableObject {
-        private let heartRateService: RestingHeartRateService
-
-        @Published var presentAlert = false
-        @Published var backgroundObserverIsOn: Bool = true {
-            didSet {
-                heartRateService.backgroundObserverQueryEnabled = backgroundObserverIsOn
-                if !backgroundObserverIsOn {
-                    presentAlert = true
-                }
-            }
-        }
-
-        @Published var fakeHeartRateValue: Double = 100.0
-
-        init(heartRateService: RestingHeartRateService = RestingHeartRateService.shared) {
-            self.heartRateService = heartRateService
-
-            if let averageHeartRate = heartRateService.averageHeartRate {
-                fakeHeartRateValue = averageHeartRate * heartRateService.threshold + 1
-            }
-
-            // This could be simpler with @AppStorage
-            backgroundObserverIsOn = heartRateService.backgroundObserverQueryEnabled
-        }
-
-        var latestHighRHRNotificationPostDate: Date? {
-            return heartRateService.latestHighRHRNotificationPostDate
-        }
-
-        func sendFakeHeartRateUpdate() {
-            let update = RestingHeartRateUpdate(date: Date(), value: fakeHeartRateValue, isRealUpdate: false)
-            heartRateService.handleDebugUpdate(update: update)
-        }
-
-        var averageHeartRate: Double? {
-            return heartRateService.averageHeartRate
-        }
-
-        var fakeUpdateDescriptionText: String {
-            return """
-            Tapping the button will make the app to process a fake resting heart rate update with a resting heart rate of \(String(format: "%.0f", (fakeHeartRateValue))) bpm. It will be posted after a short delay.
-
-            The value won't be saved to the HealthKit database and it won't affect the notifications you'd receive from Restful normally, so it's safe to test the update.
-            """
-        }
-
-        var notificationFootnoteString: String {
-            let thresholdRestingHeartRate: String
-            if let averageHeartRate = averageHeartRate {
-                thresholdRestingHeartRate = String(format: " (above %.0f bpm)", averageHeartRate * heartRateService.threshold)
-            } else {
-                thresholdRestingHeartRate = ""
-            }
-
-            return """
-            If Restful detects an elevated resting heart rate\(thresholdRestingHeartRate), you'll receive a push notification. You'll be notified only once per day about elevated resting heart rate.
-            """
-        }
-
-        var latestHighRHRNotificationDisplayString: String {
-            if let date = latestHighRHRNotificationPostDate {
-                return "Restful has notified you about elevated resting heart rate \(date.timeAgoDisplay())"
-            } else {
-                return "No elevated resting heart levels detected."
-            }
-        }
-
-        var backgroundObservationText: String {
-            if backgroundObserverIsOn {
-                return "Restful is observing the changes in your resting heart rate in the background. If this switch is off, you won't receive notifications."
-            } else {
-                return "Restful has stopped observing the cnahges in your resting heart rate. It won't send you notifications until this switch is turned on."
-            }
-        }
-
-        var highRHRIsPostedToday: Bool {
-             return heartRateService.hasPostedAboutRisingNotificationToday
-        }
-
-        var applicationVersionDisplayable: String {
-            guard let versionNumber = Bundle.main.releaseVersionNumber else {
-                return ""
-            }
-
-            if let buildNumber = Bundle.main.buildVersionNumber {
-                return "Restful version " + versionNumber + " (" + buildNumber + ")"
-            } else {
-                return "Restful version " + versionNumber
-            }
-        }
-    }
-
     var body: some View {
         VStack(alignment: .center, spacing: 12) {
             HStack {
@@ -189,14 +96,5 @@ struct InfoView: View {
 struct DebugView_Previews: PreviewProvider {
     static var previews: some View {
         InfoView(viewModel: InfoView.ViewModel(heartRateService: RestingHeartRateService.shared))
-    }
-}
-
-extension Bundle {
-    var releaseVersionNumber: String? {
-        return infoDictionary?["CFBundleShortVersionString"] as? String
-    }
-    var buildVersionNumber: String? {
-        return infoDictionary?["CFBundleVersion"] as? String
     }
 }
--- HealthMeter/View/TutorialView.swift
+++ HealthMeter/View/TutorialView.swift
@@ -18,38 +18,6 @@ struct TutorialView: View {
         case allDone
     }
 
-    class ViewModel: ObservableObject {
-        @Published var authorized = false {
-            didSet {
-                currentPhase = .allowPushNotifications
-            }
-        }
-        @Published var notificationsEnabled = false {
-            didSet {
-                currentPhase = .allDone
-            }
-        }
-        @Published var currentPhase: Phase = .authorizeHealthKit
-
-        @Published var presentNotificationsAlert = false
-
-        private let heartRateService: RestingHeartRateService
-
-        init(heartRateService: RestingHeartRateService = RestingHeartRateService.shared) {
-            self.heartRateService = heartRateService
-        }
-
-        func authorizeHealthKit() {
-            heartRateService.requestAuthorisation { [weak self] _, _ in
-                guard let self = self else { return }
-
-                DispatchQueue.main.async {
-                    self.authorized = true
-                }
-            }
-        }
-    }
-
     var body: some View {
         VStack(alignment: .leading, spacing: 12.0) {
             HStack {
