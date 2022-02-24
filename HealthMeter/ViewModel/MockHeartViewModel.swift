0887f56c9674e689620f7a070707a80608ae7bbb
Arttu Ekholm <arttu.ekholm@bitodi.fi>
2022-02-24 11:36:18 +0200

    Display histogram for the screenshots

--- HealthMeter/ViewModel/MockHeartViewModel.swift
+++ HealthMeter/ViewModel/MockHeartViewModel.swift
@@ -5,7 +5,8 @@
 //  Created by Arttu Ekholm on 16.2.2022.
 //
 
-import Foundation
+import SwiftUI
+import HealthKit
 
 class DummyHeartRateService: RestingHeartRateService {
     var average: Double?
@@ -14,11 +15,40 @@ class DummyHeartRateService: RestingHeartRateService {
         set { average = newValue }
     }
 
+    override init(userDefaults: UserDefaults = UserDefaults.standard, calendar: Calendar = Calendar.current, notificationService: NotificationService = NotificationService(), healthStore: HKHealthStore = HKHealthStore(), queryProvider: QueryProvider = QueryProvider(), queryParser: QueryParser = QueryParser()) {
+        super.init(userDefaults: userDefaults, calendar: calendar, notificationService: notificationService, healthStore: healthStore, queryProvider: queryProvider, queryParser: queryParser)
+
+        averageHeartRate = 57.0
+    }
+
     override func queryAverageRestingHeartRate(averageRHRCallback: @escaping (Result<Double, Error>) -> Void) {
-        averageRHRCallback(.success(56.0))
+        averageRHRCallback(.success(57.0))
     }
 
     override func queryLatestRestingHeartRate(completionHandler: @escaping (Result<RestingHeartRateUpdate, Error>) -> Void) {
-        completionHandler(.success(RestingHeartRateUpdate(date: .now, value: 61.0)))
+        completionHandler(.success(RestingHeartRateUpdate(date: .now.addingTimeInterval(-60*60), value: 58.0)))
+    }
+
+    override func fetchRestingHeartRateHistory(startDate: Date, completion: @escaping (Result<RestingHeartRateHistory, Error>) -> Void) {
+        let history = RestingHeartRateHistory(histogramItems: [
+            RestingHeartRateHistogramItem(item: 50, count: 1),
+            RestingHeartRateHistogramItem(item: 51, count: 0),
+            RestingHeartRateHistogramItem(item: 52, count: 3),
+            RestingHeartRateHistogramItem(item: 53, count: 2),
+            RestingHeartRateHistogramItem(item: 54, count: 4),
+            RestingHeartRateHistogramItem(item: 55, count: 6),
+            RestingHeartRateHistogramItem(item: 56, count: 8),
+            RestingHeartRateHistogramItem(item: 57, count: 12),
+            RestingHeartRateHistogramItem(item: 58, count: 5),
+            RestingHeartRateHistogramItem(item: 59, count: 4),
+            RestingHeartRateHistogramItem(item: 60, count: 3),
+            RestingHeartRateHistogramItem(item: 61, count: 1),
+            RestingHeartRateHistogramItem(item: 62, count: 0),
+            RestingHeartRateHistogramItem(item: 63, count: 1),
+            RestingHeartRateHistogramItem(item: 64, count: 3),
+            RestingHeartRateHistogramItem(item: 65, count: 1)
+
+        ])
+        completion(.success(history))
     }
 }
