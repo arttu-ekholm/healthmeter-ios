889f0dc85ea18a5f5aa22c9b269e507b740ff955
Arttu Ekholm <arttue@gmail.com>
2023-08-09 12:45:50 +0300

    Add WhatsNew kit

--- HealthMeter/HealthMeterApp.swift
+++ HealthMeter/HealthMeterApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WhatsNewKit
 
 @main
 struct HealthMeterApp: App {
@@ -13,6 +14,13 @@ struct HealthMeterApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(
+                    \.whatsNew,
+                     WhatsNewEnvironment(
+                        versionStore: UserDefaultsWhatsNewVersionStore(),
+                        whatsNewCollection: self
+                     )
+                )
         }
     }
 }
@@ -30,4 +38,37 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         }
         return true
     }
+}
+
+// MARK: - App+WhatsNewCollectionProvider
+
+extension HealthMeterApp: WhatsNewCollectionProvider {
+    /// Declare your WhatsNew instances per version
+    var whatsNewCollection: WhatsNewCollection {
+        WhatsNew(
+            version: "1.3.0",
+            title: "What's new in Restful",
+            features: [
+                WhatsNew.Feature(image: .init(systemName: "thermometer.medium", foregroundColor: .accentColor),
+                                 title: "Wrist temperature measurement",
+                                 subtitle: "Get notified when your body temperature gets elevated.")],
+            primaryAction: WhatsNew.PrimaryAction(
+                    title: "Grant access",
+                    backgroundColor: .accentColor,
+                    foregroundColor: .white,
+                    hapticFeedback: .notification(.success),
+                    onDismiss: {
+                        RestingHeartRateService.shared.requestAuthorisation { _, _ in }
+                    }
+                ),
+                // The optional secondary action that is displayed above the primary action
+                secondaryAction: WhatsNew.SecondaryAction(
+                    title: "Continue",
+                    foregroundColor: .accentColor,
+                    hapticFeedback: .selection,
+                    action: .dismiss
+                )
+        )
+    }
+
 }
