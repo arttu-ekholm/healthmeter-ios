3cf371e9c8c31c22ae13cec4b42a499fcbf2b0f2
Arttu Ekholm <arttue@gmail.com>
2023-08-15 08:53:04 +0300

    Add basic setup and teardown functions for HeartViewModel tests

--- HealthMeterTests/HeartViewModelTests.swift
+++ HealthMeterTests/HeartViewModelTests.swift
@@ -5,4 +5,22 @@
 //  Created by Arttu Ekholm on 14.8.2023.
 //
 
-import Foundation
+import XCTest
+@testable import HealthMeter
+
+class HeartViewModelTests: XCTestCase {
+    var userDefaults: UserDefaults!
+
+    override func setUp() {
+        super.setUp()
+
+        userDefaults?.removePersistentDomain(forName: #file)
+        userDefaults = UserDefaults(suiteName: #file)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        userDefaults.removePersistentDomain(forName: #file)
+    }
+}
